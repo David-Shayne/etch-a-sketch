5297f5abca66df035cff61ecf17a1befe7755880
davidmshayne <63462420+David-Shayne@users.noreply.github.com>
2025-07-29 19:08:12 +0200

    Setup dimensions of boxes

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const sizeInputBtnEle = document.getElementById("submit-canvas-size");
 const sizeInputEle = document.getElementById("canvas-size-input");
+const canvasEle = document.createElement("div");
+canvasEle.id = "canvas";
 
 sizeInputBtnEle.addEventListener("click", (e) => {
 	//Stop submit default function
 	e.preventDefault();
 	let input = e.target.previousElementSibling.value;
-	let canvasBoxNum = input * input;
-	console.log(canvasBoxNum);
+	let boxWAndHPercentage = 1 / input;
+
+	//Create
+	for (let i = 1; i <= totalBoxNum; i++) {
+		//Add a canvas block
+		let canvasBlockEle = document.createElement("div");
+		canvasBlockEle.className = "canvas-block";
+
+		//Set the appropriate width and height
+		canvasBlockEle.style.width = `${boxWAndHPercentage * 100}%`;
+		canvasBlockEle.style.height = `${boxWAndHPercentage * 100}%`;
+	}
 });
-
-//
-//
-//
-//
-const canvasEle = document.createElement("div");
-canvasEle.id = "canvas";
-
-document.body.append(canvasEle);
